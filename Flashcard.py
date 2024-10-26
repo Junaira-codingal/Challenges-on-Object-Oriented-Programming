308fd5aeb17b8a5eebd808af91b8ee08e08b86d7
Junaira <advshakerullah@gmail.com>
2024-10-26 17:29:01 +0600

    flashcard

---
+++ Flashcard.py
@@ -0,0 +1,26 @@
+class flashcard:
+    
+    def __init__(self ,word ,  meaning):
+        self.word = word
+        self.meaning = meaning
+        
+    def __str__(self):
+        return f"{self.word} ({self.meaning} )"
+flash = []
+print("welcom to flashcard application")
+
+while(True):
+    
+    word = input("enter the name you want to add to flashcard: ")
+    meaning = input("enter the meaning of the word: ")
+    
+    flash.append( flashcard(word , meaning)  )
+    
+    option = int(input("enter 0 , if you want to add another flashcard: "))
+    
+    if(option):
+        break
+    
+print("\nYour flashcards")
+for i in flash:
+    print(">", i)
